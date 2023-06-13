8a2214298b351b112d2f7ba5bde60c36c9bd8599
kkang3617 <rkdgus3617@naver.com>
2023-06-13 12:22:14 +0900

    #3. react 훅(useState, useEffect)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ChangeEvent from './component/chap01_event/ChangeEvent';
 import ClickEvent from './component/chap01_event/ClickEvent';
 import SubmitEvent from './component/chap01_event/SubmitEvent';
 import FoodList from './component/chap02_props/FoodList';
+import Counter from './component/chap03_state/Counter';
+import NickName from './component/chap03_state/NickName';
 
 //리액트에서는 하나의 컴포넌트가 여러개의 엘리먼트르 반환할 수 있다.
 //JSX를 작성할 때 return 문 안에 반드시 하나의 최상위 태그가 있어야 한다.
@@ -23,7 +25,7 @@ function App() {
   //{} 안에서 함수의 호출문이나 변수참조는 가능.
   return (
     <>
-      <FoodList />
+      <Counter />
     </>
   );
 }
